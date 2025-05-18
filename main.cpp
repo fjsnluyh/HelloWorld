b330b247b291734f07096eff4db1d7da22a0fa71
Luyh <951916215@qq.com>
2025-05-18 14:26:24 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void soft(int arr[],int size){
+	
+}
+
+int main(){
+	int arr[10] = {10,20,34,5,26,43,64,100,31,32}
+	int size = sizeof(arr)/sizeof(arr[0]);
+	soft(arr,size);
+	return 0;
+}
