a4f733ab38b57fe73be88b24f41a976e29c01807
Luyh <951916215@qq.com>
2025-05-18 16:22:11 +0800

    main.cpp add fun divfun() method

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ int addfun(int a,int b){
 	return a + b;
 }
 
+int divfun(int a,int b){
+	retrun a/b;
+}
+
 int main(){
 	int arr[10] = {10,20,34,5,26,43,64,100,31,32}
 	int size = sizeof(arr)/sizeof(arr[0]);
